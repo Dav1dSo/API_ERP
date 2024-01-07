4a30c8dd2c60d6f7d453c65c61d8beeb0479d5e3
David <soaresdavid411@gmail.com>
2024-01-07 18:12:26 -0300

    feat: configured sequelize and realized conection with database.

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import express from 'express';
 import routesProducts from './src/routes/products';
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use('/api/produtos', routesProducts);
 
-app.listen(process.env.port || 3000, () => {
-    console.log('Server running!');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
 });
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Conexão com o banco de dados PostgreSQL estabelecida!');
+    })
+    .catch((err) => {
+        console.error('Erro ao conectar ao banco de dados:', err);
+    });
+
+app.listen(process.env.PORT_SERVE || 3000, () => {
+    console.log(`Server running in port ${process.env.PORT_SERVE} !`);
+});
+
+export default sequelize;
