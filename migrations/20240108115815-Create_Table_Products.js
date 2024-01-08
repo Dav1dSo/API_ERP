2b9434921e10cc097876b2bcfe565ba6f8def9f4
David <soaresdavid411@gmail.com>
2024-01-08 10:56:10 -0300

    feat: created migrations of products.

---
+++ migrations/20240108115815-Create_Table_Products.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      cod: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+
+      name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+
+      image: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+
+      stock: {
+        type: Sequelize.INET,
+        allowNull: false,
+      },
+      
+      sold: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+
+      categorie: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
