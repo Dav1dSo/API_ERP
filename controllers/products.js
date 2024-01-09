8c10609e07b3a40e781aa5e35d4704b1747b9a92
David <soaresdavid411@gmail.com>
2024-01-09 10:49:48 -0300

    feat: created controller for find product.

--- controllers/products.js
+++ controllers/products.js
@@ -1,18 +1,29 @@
 import Product from '../models/product'
 
-const getProducts = async (req, res) => {
-  
-  let options = {
-    limit: req.query.limit ? req.query.limit : 1000,
-  };
+const GetProducts = async (req, res) => {
+
+  const limited = req.query.limit ? req.query.limit : 1000;
 
   try {
-    const products = await Product.findAll(options);
+    const products = await Product.findAll({ limit: limited });
     res.status(200).json(products);
   } catch (error) {
-    console.error('Erro ao buscar os produtos:', error);
     res.status(500).json('Erro ao buscar os produtos');
   }
 };
 
-export default getProducts;
+const FindProduct = async (req, res) => {
+
+  const codeProduct = req.query.codProduct;
+  const where = { cod: codeProduct };
+
+  try {
+    const product = await Product.findAll({ where: where });
+    res.status(200).json(product);
+  } catch (error) {
+    console.error('Erro ao buscar os produtos:', error);
+    res.status(500).json('Error ao buscar produto!');
+  }
+}
+
+export { GetProducts, FindProduct };   
