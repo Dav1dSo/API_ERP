b757c108f1c33a738ada0aa5a9838c54a971dfc1
David <soaresdavid411@gmail.com>
2024-01-09 11:45:45 -0300

    feat: create route dor controller get product by category.

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { GetProducts, FindProduct } from '../controllers/products';
+import { GetProducts, FindProduct, GetProductsByCategorie } from '../controllers/products';
 
 const route = express.Router();
 
 route.get('/', GetProducts);
 route.get('/getProduct', FindProduct);
+route.get('/getProduct/filter_categorie/:category', GetProductsByCategorie);
 
 export default route; 
