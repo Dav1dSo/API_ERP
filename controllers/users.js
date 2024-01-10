e7ada9d1c3290775a744007db9da7050cdb5dcbd
David <soaresdavid411@gmail.com>
2024-01-10 17:10:29 -0300

    feat: created function register user.

---
+++ controllers/users.js
@@ -0,0 +1,15 @@
+import Users from '../models/users';
+import generateUUID from '../functions/generateUUID';
+
+const CreateUser = async (req, res) => {
+    try {
+        const { userName, email, password } = req.body;
+        const idUser = generateUUID();
+        await Users.create({ idUser, userName, email, password });
+        res.status(200).json('Usuário cadastrado com sucesso!');
+    } catch (error) {
+        res.status(500).json("Error ao criar novo usuário" + error);
+    }
+}
+
+export default CreateUser;  
