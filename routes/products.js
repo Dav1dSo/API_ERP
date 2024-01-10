184a56be79eee2bb67564b69b72a036d54b0a09f
David <soaresdavid411@gmail.com>
2024-01-10 10:36:54 -0300

    feat: created route for product filter by price.

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { GetProducts, FindProduct, GetProductsByCategorie, CreateProduct } from '../controllers/products';
+import { GetProducts, FindProduct, GetProductsByCategorie, CreateProduct, GetProductsByValue } from '../controllers/products';
 
 const route = express.Router();
 
@@ -7,5 +7,6 @@ route.get('/', GetProducts);
 route.get('/getProduct', FindProduct);
 route.get('/getProduct/filter_categorie/:category', GetProductsByCategorie);
 route.post('/createProduct', CreateProduct);
+route.get('/filterValue', GetProductsByValue);
 
 export default route; 
