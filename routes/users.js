17b9e3cd82d39e2065f53be1bb7c75943eb13c18
David <soaresdavid411@gmail.com>
2024-01-10 17:08:08 -0300

    feat: created route for users.

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 import express  from "express";
+import CreateUser from "../controllers/users";
 const route = express.Router();
 
-route.get('/users/', )
+route.post('/createUser', CreateUser)
+
+export default route;
