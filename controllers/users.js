c9070b4a149cf27ee87c388919f8698f956ffa80
David <soaresdavid411@gmail.com>
2024-01-10 18:16:07 -0300

    feat: created password encryption.

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,13 @@
 import Users from '../models/users';
 import generateUUID from '../functions/generateUUID';
+import bcrypt from 'bcrypt';
 
 const CreateUser = async (req, res) => {
     try {
         const { userName, email, password } = req.body;
         const idUser = generateUUID();
-        await Users.create({ idUser, userName, email, password });
+        let hashedPassword = await bcrypt.hash(password, 10);
+        await Users.create({ idUser, userName, email, password: hashedPassword });
         res.status(200).json('Usuário cadastrado com sucesso!');
     } catch (error) {
         res.status(500).json("Error ao criar novo usuário.");
