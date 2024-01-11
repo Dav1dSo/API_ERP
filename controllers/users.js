6fdac3688e8f0352440e7b39c67cd75f0e1888e1
David <soaresdavid411@gmail.com>
2024-01-11 19:14:16 -0300

    feat: added validation for data users in updated.

--- controllers/users.js
+++ controllers/users.js
@@ -15,21 +15,23 @@ const CreateUser = async (req, res) => {
 };
 
 const UpdatedUser = async (req, res) => {
-    
+
+    const ValidatePassword = (password) => password.length >= 8;
+    const ValidateUserName = (userName) => userName.length > 2;
+
     try {
         const { idUser, userName, email, password } = req.body;
         const user = await Users.findByPk(idUser);
-    
-        user.userName = userName;
+
+        !ValidatePassword(password) ? res.status(500).json('Senha inválida! Mínimo de 8 caracteres.') : user.password = await bcrypt.hash(password, 10);;
+        !ValidateUserName(userName) ? res.status(500).json('Nome de usuário nulo ou muito curto!') : user.userName = userName;
         user.email = email;
-        const hashPassword = await bcrypt.hash(password, 10);
-        user.password = hashPassword;
 
         await user.save();
         res.status(200).json('Usuário atualizado com sucesso!');
     } catch (error) {
-        res.status(500).json('Não foi possível atualiar informações de usuário!');        
+        res.status(500).json('Não foi possível atualiar informações de usuário!' + error);
     }
-}
+} 
 
-export { CreateUser, UpdatedUser };
+export { CreateUser, UpdatedUser };  
