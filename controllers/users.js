1cae7378cc1dde7ba175fe8e9054a717f39fcaaf
David <soaresdavid411@gmail.com>
2024-01-11 11:38:32 -0300

    feat: created controller for updated user.

--- controllers/users.js
+++ controllers/users.js
@@ -12,6 +12,24 @@ const CreateUser = async (req, res) => {
     } catch (error) {
         res.status(500).json("Error ao criar novo usuário.");
     }
+};
+
+const UpdatedUser = async (req, res) => {
+    
+    try {
+        const { idUser, userName, email, password } = req.body;
+        const user = await Users.findByPk(idUser);
+    
+        user.userName = userName;
+        user.email = email;
+        const hashPassword = await bcrypt.hash(password, 10);
+        user.password = hashPassword;
+
+        await user.save();
+        res.status(200).json('Usuário atualizado com sucesso!');
+    } catch (error) {
+        res.status(500).json('Não foi possível atualiar informações de usuário!');        
+    }
 }
 
-export default CreateUser;  
+export { CreateUser, UpdatedUser };
